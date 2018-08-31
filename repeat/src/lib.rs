230fdf3da88206c47c8e7f71dc7570b46c839f67
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-08-31 03:10:08 +0200

    Fix typo in test name
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- repeat/src/lib.rs
+++ repeat/src/lib.rs
@@ -30,7 +30,7 @@ mod tests {
 	}
 
 	#[test]
-	fn repeat_often_results_in_a_string_of_the_lenght_times_repeat() {
+	fn repeat_often_results_in_a_string_of_the_length_times_repeat() {
 		let my_string = String::from("hello");
 		let length = my_string.len();
 		assert_eq!(repeat(200, my_string).len(), 200 * length);
