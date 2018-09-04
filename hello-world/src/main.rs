feb15b21996d32bdc1be03b16779040ff1c0e6f5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-09-04 22:04:21 +0200

    Add main for is_palindrome
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,8 +1,13 @@
+extern crate is_palindrome;
 extern crate repeat;
 
 fn main() {
+	println!("Hello {}", repeat::repeat(5, String::from("world")));
+
+	let my_string = String::from("rentner");
 	println!(
-		"Hello {}",
-		repeat::repeat(5, String::from("world"))
-	);
+		"{} is palindrome? {}",
+		my_string,
+		is_palindrome::is_palindrome(String::from("rentner"))
+	)
 }
