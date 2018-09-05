f4d864a41e59f075e24c7911261fbab6d7c8679c
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-09-06 00:12:05 +0200

    Understand borrowing a bit
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,7 +4,8 @@ extern crate repeat;
 fn main() {
 	println!("Hello {}", repeat::repeat(5, String::from("world")));
 
-	let my_string = { String::from("rentner") };
-	print!("{} is palindrome?", my_string);
-	println!(" {}", is_palindrome::is_palindrome(my_string))
+	let my_string = String::from("rentner");
+	let my_string_is_palindrome = is_palindrome::is_palindrome(&my_string);
+
+	println!("{} is palindrome? {}", my_string, my_string_is_palindrome)
 }
--- is-palindrome/src/lib.rs
+++ is-palindrome/src/lib.rs
@@ -1,6 +1,6 @@
-pub fn is_palindrome(text: String) -> bool {
+pub fn is_palindrome(text: &String) -> bool {
 	let reversed = text.chars().rev().collect::<String>();
-	reversed == text
+	reversed == text.to_string()
 }
 
 #[cfg(test)]
@@ -9,25 +9,25 @@ mod tests {
 
 	#[test]
 	fn is_palindrome_returns_true_for_empty_string() {
-		let test_string = String::from("");
+		let test_string = &String::from("");
 		assert_eq!(is_palindrome(test_string), true);
 	}
 
 	#[test]
 	fn is_palindrome_returns_true_for_a_single_character_string() {
-		let test_string = String::from("a");
+		let test_string = &String::from("a");
 		assert_eq!(is_palindrome(test_string), true);
 	}
 
 	#[test]
 	fn is_palindrome_returns_false_for_non_palindromes() {
-		let test_string = String::from("abc");
+		let test_string = &String::from("abc");
 		assert_eq!(is_palindrome(test_string), false);
 	}
 
 	#[test]
 	fn is_palindrome_returns_true_for_strings_consisting_of_single_character() {
-		let test_string = String::from("aaaaaaaa");
+		let test_string = &String::from("aaaaaaaa");
 		assert_eq!(is_palindrome(test_string), true);
 	}
 }
