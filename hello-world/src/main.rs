9f34a1140f62f3606d8e785b568e276faeb538f8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-09-05 23:57:03 +0200

    Use some weird strategy to get rid of move error
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -4,10 +4,7 @@ extern crate repeat;
 fn main() {
 	println!("Hello {}", repeat::repeat(5, String::from("world")));
 
-	let my_string = String::from("rentner");
-	println!(
-		"{} is palindrome? {}",
-		my_string,
-		is_palindrome::is_palindrome(String::from("rentner"))
-	)
+	let my_string = { String::from("rentner") };
+	print!("{} is palindrome?", my_string);
+	println!(" {}", is_palindrome::is_palindrome(my_string))
 }
