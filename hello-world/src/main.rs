c88c612b3d563e970909c8375a6958ec7d313ca6
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-09-08 22:46:42 +0200

    Add possibility to provide own word for palindrome check
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,10 +1,22 @@
 extern crate is_palindrome;
 extern crate repeat;
 
+use std::io;
+use std::io::Write;
+
 fn main() {
 	println!("Hello {}", repeat::repeat(5, String::from("world")));
 
-	let my_string = String::from("rentner");
+	let mut my_string = String::new();
+	print!("Check if the following word is a palindrome: ");
+	io::stdout().flush().expect("Failed to write line");
+
+	io::stdin()
+		.read_line(&mut my_string)
+		.expect("Failed to read line");
+
+	my_string = my_string.trim().to_string();
+
 	let my_string_is_palindrome = is_palindrome::is_palindrome(&my_string);
 
 	println!("{} is palindrome? {}", my_string, my_string_is_palindrome)
