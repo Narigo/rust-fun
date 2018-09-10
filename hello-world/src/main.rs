bc90084fcabee853509c01db2692b8c0bd0d1360
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-09-10 03:28:05 +0200

    Move the loop into own function
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -8,18 +8,22 @@ fn main() {
 	println!("Hello {}", repeat::repeat(5, String::from("world")));
 
 	loop {
-		let mut my_string = String::new();
-		print!("Check if the following word is a palindrome: ");
-		io::stdout().flush().expect("Failed to write line");
-
-		io::stdin()
-			.read_line(&mut my_string)
-			.expect("Failed to read line");
-
-		my_string = my_string.trim().to_string();
-
-		let my_string_is_palindrome = is_palindrome::is_palindrome(&my_string);
-
-		println!("{} is palindrome? {}", my_string, my_string_is_palindrome)
+		check_palindrome()
 	}
+}
+
+fn check_palindrome() {
+	let mut my_string = String::new();
+	print!("Check if the following word is a palindrome: ");
+	io::stdout().flush().expect("Failed to write line");
+
+	io::stdin()
+		.read_line(&mut my_string)
+		.expect("Failed to read line");
+
+	my_string = my_string.trim().to_string();
+
+	let my_string_is_palindrome = is_palindrome::is_palindrome(&my_string);
+
+	println!("{} is palindrome? {}", my_string, my_string_is_palindrome)
 }
