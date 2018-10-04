4a2f2d33ff2d0439e2117833b0eb5f0e281e0b64
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-10-05 01:46:56 +0200

    Walk groups recursively
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- keepass-diff/src/main.rs
+++ keepass-diff/src/main.rs
@@ -1,27 +1,51 @@
 extern crate keepass;
 
-use keepass::{Database, Node, OpenDBError};
+use keepass::{Database, Group, OpenDBError};
 use std::fs::File;
 use std::path::Path;
 
 fn main() {
-    let db = File::open(Path::new("test/test.kdbx"))
+    let db = File::open(Path::new("keepass-diff/test/test.kdbx"))
         .map_err(|e| OpenDBError::from(e))
         .and_then(|mut db_file| Database::open(&mut db_file, "demopass"))
         .unwrap();
 
     // Iterate over all Groups and Nodes
-    for node in &db.root {
-        match node {
-            Node::Group(g) => {
-                println!("Saw group '{0}'", g.name);
-            }
-            Node::Entry(e) => {
-                let title = e.get_title().unwrap();
-                let user = e.get_username().unwrap();
-                // let pass = e.get_password().unwrap();
-                println!("Entry '{0}': '{1}' : ---", title, user);
-            }
-        }
+    let accumulated = check_group(&mut Vec::new(), &mut Vec::new(), db.root);
+
+    for entry in accumulated {
+        println!("{:?}", entry);
     }
+}
+
+fn check_group(
+    accumulated: &mut Vec<(Vec<String>, String, String, String)>,
+    parents: &mut Vec<String>,
+    current_group: Group,
+) -> Vec<(Vec<String>, String, String, String)> {
+    println!("looking at group {}", current_group.name);
+    parents.push(current_group.name);
+    accumulated.push((
+        parents.clone(),
+        String::from(""),
+        String::from(""),
+        String::from(""),
+    ));
+    for entry in current_group.entries {
+        accumulated.push((
+            parents.clone(),
+            entry.get_title().unwrap().to_string(),
+            entry.get_username().unwrap().to_string(),
+            entry.get_password().unwrap().to_string(),
+        ))
+    }
+    for next_parent in current_group.child_groups {
+        let children = check_group(
+            &mut accumulated.clone(),
+            &mut parents.clone(),
+            next_parent,
+        );
+        accumulated.append(&mut children.clone())
+    }
+    accumulated.clone()
 }
