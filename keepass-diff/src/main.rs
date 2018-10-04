1998cf8e3f78b30ffcf6e6c33eb969875ae7e678
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-10-04 23:54:15 +0200

    Start keepass-diff by trying out example of keepass crate
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- keepass-diff/src/main.rs
+++ keepass-diff/src/main.rs
@@ -1,3 +1,27 @@
+extern crate keepass;
+
+use keepass::{Database, Node, OpenDBError};
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let db = File::open(Path::new("test/test.kdbx"))
+        .map_err(|e| OpenDBError::from(e))
+        .and_then(|mut db_file| Database::open(&mut db_file, "demopass"))
+        .unwrap();
+
+    // Iterate over all Groups and Nodes
+    for node in &db.root {
+        match node {
+            Node::Group(g) => {
+                println!("Saw group '{0}'", g.name);
+            }
+            Node::Entry(e) => {
+                let title = e.get_title().unwrap();
+                let user = e.get_username().unwrap();
+                // let pass = e.get_password().unwrap();
+                println!("Entry '{0}': '{1}' : ---", title, user);
+            }
+        }
+    }
 }
