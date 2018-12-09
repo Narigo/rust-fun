32ff2e475065dc6c5b2d316104cc37eb617a2aa8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-12-09 23:24:06 +0100

    Draw clock / wheel like
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- wasm-test/www/index.js
+++ wasm-test/www/index.js
@@ -68,7 +68,9 @@ export function run() {
       ctx.arc(x, y, radius, angle_min, angle_max);
     } else {
       ctx.moveTo(x, y);
-      ctx.lineTo(x + radius + angle_min - 180, y + radius + angle_max - 180);
+      ctx.lineTo(x + Math.cos(angle_max - angle_min) * radius, y + Math.sin(angle_max - angle_min) * radius);
+      ctx.moveTo(x, y);
+      ctx.lineTo(x + Math.cos(angle_max - angle_min) * radius / 2, y + Math.sin(angle_max - angle_min) * radius / 2);
     }
     ctx.stroke();
 
